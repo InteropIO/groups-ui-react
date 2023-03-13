b7b85a00128a0ff0aa5f69f00e2f6d8fee3b49c8
PIRINSOFT\iatanasova <irina.atanasova@tick42.com>
2023-03-13 15:37:20 +0200

    added loading animation component

---
+++ src/defaultComponents/loadingAnimation/LoadingAnimation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoadingAnimation: React.FC = () => {
+    return (
+        <div className="t42-loading-animation">
+            <div className="t42-loading active">
+                <div className="t42-loading-text">Loading...</div>
+            </div>
+        </div>
+    );
+}
+
+export default LoadingAnimation;
